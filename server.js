188e7c2bebc243f06f3291c360f6cf324ffdc60a
Nermine khadhraoui <nerminekhadhraoui4@gmail.com>
2025-03-14 13:43:03 +0100

    modifiaction

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Import des modules
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -6,12 +5,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
-// Initialisation de l'application
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// Connexion à MongoDB
 mongoose.connect("mongodb+srv://nerminekh:nermine321@tpnode.3xapw.mongodb.net/TPnode?retryWrites=true&w=majority", {
 })
   .then(() => console.log(" Connexion à MongoDB réussie !"))
@@ -49,35 +46,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// WebSocket
-io.on("connection", (socket) => {
-  console.log("Un utilisateur connecté");
+// Route pour récupérer tous les messages : evan
 
-  socket.on("message", async (data) => {
-    const nextId = await getNextSequence("messages");
-    const newMessage = new Message({ ...data, id: nextId });
-  
-    await newMessage.save();
-    io.emit("message", newMessage);
-  });
-  
-  socket.on("disconnect", () => {
-    console.log("Un utilisateur déconnecté");
-  });
-});
+// Route pour supprimer un message par ID ; soufiane
 
-// Routes API
-app.get("/logs", async (req, res) => {
-    const messages = await Message.find({}, { _id: 0, __v: 0 }); // Exclut _id et __v
-    res.json(messages);
-  });
+// Gestion des connexions WebSocket: mustapha
 
-app.delete("/logs/:id", async (req, res) => {
-  await Message.findByIdAndDelete(req.params.id);
-  res.json({ message: "Log supprimé" });
-});
 
-// Lancement du serveur
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Serveur en écoute sur http://localhost:${PORT}`);
